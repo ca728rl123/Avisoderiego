d0dbbf0fb54f639783c9e0dfea0159db968a9bf8
ca728rl123 <99198903+ca728rl123@users.noreply.github.com>
2022-03-24 23:30:28 -0500

    Update Muestreodedatos.py

--- Codigoenpython/Muestreodedatos.py
+++ Codigoenpython/Muestreodedatos.py
@@ -3,10 +3,15 @@ import math
 
 wac = pd.read_excel("https://github.com/ca728rl123/IrrigacionAutomatica/blob/main/Codigoenpython/tabla%20de%20datos.xlsx?raw=true")
 df = pd.DataFrame(wac)
- 
-print(df)
+s=str(input("¿Quiere ver la lista de datos?: "))
+if s=="Si":
+ print(df)
+else:
+ print("Ahora elija hasta la fila que quiera visualizar")
+
 
 for p in range(50):
   p=int(input())
   print(df.iloc[0:p])
   break
+
